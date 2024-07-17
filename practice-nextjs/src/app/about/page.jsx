42ee9cdc6dfad4b7478725d1850e519457611fed
Deric Jojo <73828512+JackitudilinksG@users.noreply.github.com>
2024-07-17 23:25:51 +0530

    initial commit
    basic commerce webpage

---
+++ practice-nextjs/src/app/about/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+
+const page = () => {
+  return (
+    <div className='max-w-5xl text-center mt-3'>
+      <nav className='text-white font-inter  font-medium   text-center'>
+        <div className=" flex align-center justify-between p-6 bg-slate-800 rounded-md">
+          <Link href="/" className= 'hover:text-blue-200'>Home</Link>
+          <Link href="/shop" className='hover:text-blue-200'>Shop</Link>
+          <Link href="/contact" className='hover:text-blue-200'>Contact</Link>
+          <Link href="/about" className='hover:text-blue-200'>About</Link>
+        </div>
+      </nav>
+      <br />
+      <h1 className=" font-inter font-extrabold text-6xl">This is a <span className=" text-green-400">About</span> page</h1>
+      <p className="bg-white text-black p-7 text-center mt-7 rounded-md ">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur qui aspernatur doloribus dolorem impedit asperiores.
+         Velit dolor maxime fugiat eaque maiores tempore! Unde ea soluta eaque aliquid, mollitia animi vel!
+      </p>
+    </div>
+  )
+}
+
+export default page
---
+++ practice-nextjs/src/app/shop/page.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Shop = () => {
+  return (
+    <div className='max-w-5xl text-center mt-3'>
+      <nav className='  text-white font-inter  font-medium   text-center'>
+        <div className=" flex align-center justify-between p-6 bg-slate-800 rounded-md">
+          <Link href="/" className= 'hover:text-blue-200'>Home</Link>
+          <Link href="/shop" className='hover:text-blue-200'>Shop</Link>
+          <Link href="/contact" className='hover:text-blue-200'>Contact</Link>
+          <Link href="/about" className='hover:text-blue-200'>About</Link>
+        </div>
+      </nav>
+      <br/>
+      <h1 className=" font-inter font-extrabold text-6xl">This is a <span className=" text-pink-400">Shop</span> page</h1>
+      <p className="bg-white text-black p-7 text-center mt-7 rounded-md ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur qui aspernatur doloribus dolorem impedit asperiores.
+           Velit dolor maxime fugiat eaque maiores tempore! Unde ea soluta eaque aliquid, mollitia animi vel!
+      </p>
+      <div className='flex align-center justify-between flex-row mt-8 gap-10 p-4 text-left'>
+        <div className='bg-white w-64'>
+          <Image src="/cr7_phone.jpg" width={300} height={300} className='bg-gray-400'/>
+          <p className='text-black font-bold text-xl pl-4 pt-4'>BIG FONT</p>
+          <p className='text-black pl-4 pb-4'>small font</p>
+        </div>
+
+        <div className='bg-white w-64'>
+          <Image src="/cr7_phone.jpg" width={300} height={300} className='bg-gray-400'/>
+          <p className='text-black font-bold text-xl pl-4 pt-4'>BIG FONT</p>
+          <p className='text-black pl-4 pb-4'>small font</p>
+        </div>
+
+        <div className='bg-white w-64'>
+          <Image src="/cr7_inverse.jpg" width={300} height={300} className='bg-gray-400'/>
+          <p className='text-black font-bold text-xl pl-4 pt-4'>BIG FONT</p>
+          <p className='text-black pl-4 pb-4'>small font</p>
+        </div>
+
+        <div className='bg-white w-64'>
+          <Image src="/nyc_phone.jpg" width={300} height={300} className='bg-gray-400'/>
+          <p className='text-black font-bold text-xl pl-4 pt-4'>BIG FONT</p>
+          <p className='text-black pl-4 pb-4'>small font</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Shop
